4a4cf303ea2385ddd5e435e1d4678adaf39ebb7e
Valeri Karpov <valkar207@gmail.com>
2017-05-20 20:42:19 -0600

    chore: remove co and just use async/await

--- test/collection.test.js
+++ test/collection.test.js
@@ -5,15 +5,13 @@ const Collection = require('../lib/collection');
 const { MongoClient, ObjectId } = require('mongodb');
 const assert = require('assert');
 
-require('co-mocha')(require('mocha'));
-
 describe('Collection', function() {
   let db;
 
-  before(function*() {
-    db = yield MongoClient.connect('mongodb://localhost:27017/monogram');
+  before(async function() {
+    db = await MongoClient.connect('mongodb://localhost:27017/monogram');
 
-    yield db.dropDatabase();
+    await db.dropDatabase();
   });
 
   describe('#insertOne()', function() {
@@ -25,15 +23,15 @@ describe('Collection', function() {
       x: 'number'
     }).compile('TestType');
 
-    it('works', function*() {
+    it('works', async function() {
       const Test = new Collection(db.collection('Test'), TestType);
-      const res = yield Test.insertOne({ x: 1 });
+      const res = await Test.insertOne({ x: 1 });
       assert.equal(res.result.ok, 1);
       assert.equal(res.result.n, 1);
 
       let threw = false;
       try {
-        yield Test.insertOne({ x: 'not a number' });
+        await Test.insertOne({ x: 'not a number' });
       } catch (error) {
         threw = true;
         assert.ok(error.message.indexOf('not a number') !== -1, error.message);
@@ -42,7 +40,7 @@ describe('Collection', function() {
       assert.ok(threw);
     });
 
-    it('allows transforming result', function*() {
+    it('allows transforming result', async function() {
       const Test = new Collection(db.collection('Test'));
       Test.action$ = Test.action$.map(op => {
         return Object.assign(op, {
@@ -50,12 +48,12 @@ describe('Collection', function() {
         });
       });
 
-      const res = yield Test.insertOne({ x: 1 });
+      const res = await Test.insertOne({ x: 1 });
       assert.equal(res.ok, 1);
       assert.equal(res.n, 1);
     });
 
-    it('allows transforming errors', function*() {
+    it('allows transforming errors', async function() {
       const Test = new Collection(db.collection('Test'));
       Test.action$ = Test.action$.map(op => {
         return Object.assign(op, {
@@ -65,11 +63,11 @@ describe('Collection', function() {
         });
       });
 
-      yield Test.insertOne({ _id: 1 });
+      await Test.insertOne({ _id: 1 });
 
       let threw = false;
       try {
-        yield Test.insertOne({ _id: 1 });
+        await Test.insertOne({ _id: 1 });
       } catch (error) {
         threw = true;
         assert.equal(error.message, 'woops!');
