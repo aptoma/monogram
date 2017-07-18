63718b8dbcd5ddffaa362c4df1a3697af422bbc3
Valeri Karpov <valkar207@gmail.com>
2017-07-17 21:23:09 -0700

    chore: add better docs

---
+++ HEADER.md
@@ -0,0 +1,10 @@
+# monogram
+
+Action-based anti-ODM for MongoDB and Node.js
+
+## Usage
+
+```javascript
+const { connect } = require('monogram');
+const db = await connect('mongodb://localhost:27017/test');
+```
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Database = require('./lib/database');
 
 exports.Collection = Collection;
 exports.Database = Database;
+exports.ObjectId = require('mongodb').ObjectId;
 
 exports.connect = function connect(uri, options) {
   return Database.connect(uri, options);
